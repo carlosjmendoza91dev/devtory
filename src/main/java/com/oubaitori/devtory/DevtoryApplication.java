0245a86e632c0e1041973d94fd90acff500849c5
Carlos Mendoza <82301164+carlosjmendoza91dev@users.noreply.github.com>
2024-03-24 10:22:12 -0300

    Initial commit

---
+++ src/main/java/com/oubaitori/devtory/DevtoryApplication.java
@@ -0,0 +1,13 @@
+package com.oubaitori.devtory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oubaitori/devtory/DevtoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oubaitori.devtory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
